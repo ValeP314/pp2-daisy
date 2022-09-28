e68b712542bba0f6de2ac8c2702cfd0901ad60e1
ValeP314 <valeriaparisani@hotmail.com>
2022-09-28 14:29:07 +0000

    General tidy up after meeting with mentor

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ const playButton = document.getElementById("button");
 const flyDown = document.getElementById("fly-down");
 const bee = document.getElementById("bee");
 const daisy = document.getElementById("daisy");
+const scoreDisplay = document.getElementById("score");
+let seconds = 10;
+let timer;
 
 // Wait for the DOM to finish loading before running the game
 document.addEventListener("DOMContentLoaded", function() {
@@ -16,6 +19,13 @@ function startGame(){
   } else {
     daisy.style.visibility = "visible";  
   }
+
+  //Reset the game-area, in case this is a game restart
+  score = 0;
+  scoreDisplay.innerText = score;
+  bee.style.display = ""  
+  daisy.style.animation = "";
+  daisy.style.display = "";  
 
   // Change the text on the paragraph and the button
   document.getElementById("start").innerHTML = "Let's play!";
@@ -32,6 +42,7 @@ function startGame(){
 
   // Add the collision detection
   checkCollision ();
+
 }
 
 function fly(){
@@ -47,18 +58,22 @@ function fly(){
 function checkCollision (){
   
   //set interval to check position
-  var collision = false;
+  let collision = false;
   let checkLand = setInterval(function() {
 
-    // get current bee Y position
+    // get current bee X and Y position
     let beeBottom = parseInt(window.getComputedStyle(bee).getPropertyValue("bottom"));
+    let beeRight = parseInt(window.getComputedStyle(bee).getPropertyValue("right"));
+    let beeLeft = parseInt(window.getComputedStyle(bee).getPropertyValue("left"));
   
-    // get current flower X position
+    // get current flower X and Y position
     let daisyLeft = parseInt(window.getComputedStyle(daisy).getPropertyValue("left"));
+    let daisyRight = parseInt(window.getComputedStyle(daisy).getPropertyValue("right"));
+    let daisyHeight = parseInt(window.getComputedStyle(daisy).getPropertyValue("height"));
+    let daisyBottom = parseInt(window.getComputedStyle(daisy).getPropertyValue("bottom"));
 
     // detect collision and increment score
-    let score = 0;
-    if (daisyLeft<50 && daisyLeft>0 && beeBottom<=50) {
+    if (daisyLeft<beeRight && daisyRight>beeLeft && beeBottom<(daisyBottom + daisyHeight)) {
       collision = true;
       console.log(collision);
       incrementScore();
@@ -98,15 +113,11 @@ function checkCollision (){
 }
 
 function incrementScore (){
-  let oldScore = parseInt(document.getElementById("score").innerText);
-  
-  document.getElementById("score").innerText = ++oldScore;
+  score += 1;
+  scoreDisplay.innerText = score;
 }
 
 // set 1-minute timer to establish max time
-var seconds = 59;
-var timer;
-
 function startCountDown (){
   if(!timer) {
     timer = window.setInterval(function() {
@@ -143,10 +154,4 @@ function gameOver (){
   document.getElementById("start").innerHTML = "Click below to play again";
   playButton.style.visibility = "visible"
   playButton.innerHTML = "Play again";
-  
-  
 }
-
-// Add event listener to Start button to start the game
-playButton.addEventListener("click", startGame);
-  startGame();
